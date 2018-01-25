4fdee765c12c95209a52cb81a58ea0c7c4862284
ukohank517 <ukohank517@gmail.com>
2018-01-25 17:26:04 +0900

    [wip] #16 algorithm一覧をgetしようとしている

--- src/main/java/com/projectdijkstra/web/service/AlgorithmService.java
+++ src/main/java/com/projectdijkstra/web/service/AlgorithmService.java
@@ -1,7 +1,10 @@
 package com.projectdijkstra.web.service;
 
+import java.util.List;
+
 import com.projectdijkstra.web.model.Algorithm;
 
 public interface AlgorithmService {
   public Algorithm findAlgorithmByAlgorithmId(Integer algorithmId);
+  public List<Algorithm> findAlgorithmList();
 }
--- src/main/java/com/projectdijkstra/web/service/AlgorithmServiceImpl.java
+++ src/main/java/com/projectdijkstra/web/service/AlgorithmServiceImpl.java
@@ -1,5 +1,9 @@
 package com.projectdijkstra.web.service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -16,10 +20,21 @@ public class AlgorithmServiceImpl implements AlgorithmService {
 
   @Value("${dijkstra.api.host}")
   private String apiHost;
-
+  
+  @Override
   public Algorithm findAlgorithmByAlgorithmId(Integer algorithmId) {
     UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host(apiHost)
         .path("/algorithms").queryParam("algorithm_id", algorithmId).build();
     return restTemplate.getForObject(uriComponents.toUriString(), Algorithm.class);
+  };//;←意味ある？
+  
+  @Override
+  public List<Algorithm> findAlgorithmList(){
+	UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host(apiHost)
+			.path("/algorithms/list").build();
+	return Arrays.stream(restTemplate.getForObject(uriComponents.toUriString(), Algorithm[].class))
+			.collect(Collectors.toList());
   };
+  
+
 }
