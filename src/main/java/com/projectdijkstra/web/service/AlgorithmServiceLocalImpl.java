e1227d3ea52c443058ee414a3554ade4cefd329a
ukohank517 <ukohank517@gmail.com>
2018-01-26 10:46:05 +0900

    [add] #16 ローカル用algorithm一覧を用意

--- src/main/java/com/projectdijkstra/web/service/AlgorithmServiceLocalImpl.java
+++ src/main/java/com/projectdijkstra/web/service/AlgorithmServiceLocalImpl.java
@@ -1,5 +1,8 @@
 package com.projectdijkstra.web.service;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import com.projectdijkstra.web.model.Algorithm;
@@ -12,5 +15,13 @@ public class AlgorithmServiceLocalImpl implements AlgorithmService {
   public Algorithm findAlgorithmByAlgorithmId(Integer algorithmId) {
     return Algorithm.builder().algorithmId(algorithmId).name("動的計画法").build();
   }
+  
+  @Override
+  public List<Algorithm> findAlgorithmList(){
+	List<Algorithm> algorithms = new LinkedList<>();
+	algorithms.add(Algorithm.builder().algorithmId(1).name("アルゴリズム").build());
+	return algorithms;
+  }
 
+  
 }
