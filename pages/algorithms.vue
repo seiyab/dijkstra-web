296708db6c52cf75251eb578c0fc2e515585d710
seiya <r108338@yahoo.co.jp>
2018-05-19 15:41:45 +0900

    implement /algorithms (still remains layout issure)

---
+++ pages/algorithms.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="page-header">
+      <h1>アルゴリズムから問題を探す</h1>
+    </div>
+    <div class="list-group">
+      <a
+        v-for="algorithm in algorithms"
+        class="list-group-item col-md-4 col-sm-6 col-xs-12"
+        v-bind:href="'/serp_task?algorithm_id=' + algorithm.algorithm_id"
+        v-text="algorithm.name">
+        アルゴリズム
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from '~/components/Navigation.vue'
+import axios from 'axios'
+
+export default {
+  components: {
+    Navigation
+  },
+  async asyncData () {
+    const algorithms = await axios.get('http://localhost:8080/algorithms/list').then(({data}) => data)
+    return {
+      algorithms
+    }
+  }
+}
+</script>
+
