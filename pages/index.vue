307e2f8005fbb6fdffc125a3e3636cdc3e4a269e
seiya <r108338@yahoo.co.jp>
2018-05-19 15:41:22 +0900

    use layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
-  <div>
-    <navigation/>
-    <div class="container">
-	    <div class="page-header">
-	    	<h1>Project Dijkstra</h1>
-	    </div>
-	    <p>Project Dijkstraは、各アルゴリズムに関する例題検索によってアルゴリズム習得への最短経路を提供するサービスです。</p>
-	    <h2>新着情報</h2>
-	    <p>新着情報はありません</p>
-	    <h2>使い方</h2>
-	    <p>ページ上部の「Algorithm」をクリックし、例題を探したいアルゴリズムを探してください。</p>
+  <div class="container">
+    <div class="page-header">
+      <h1>Project Dijkstra</h1>
     </div>
+    <p>Project Dijkstraは、各アルゴリズムに関する例題検索によってアルゴリズム習得への最短経路を提供するサービスです。</p>
+    <h2>新着情報</h2>
+    <p>新着情報はありません</p>
+    <h2>使い方</h2>
+    <p>ページ上部の「Algorithm」をクリックし、例題を探したいアルゴリズムを探してください。</p>
   </div>
 </template>
 
